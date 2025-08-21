cb2dc0240c11a22d69f808cfb5cff9c7307716c0
Michael <michael.mapanao@gmail.com>
2025-08-21 15:52:56 -0700

    prettified code and added navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,80 @@
+import { cn } from "@/lib/utils";
+import { useEffect } from "react";
+import { useState } from "react";
+
 const navItems = [
-    {name: "Home", href: ""}
-]
+  { name: "Home", href: "#home" },
+  { name: "About", href: "#about" },
+  { name: "Skills", href: "#skills" },
+  { name: "Projects", href: "#projects" },
+  { name: "Contact", href: "#contact" },
+];
 
 export const NavBar = () => {
-    <div></div>
-}
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.screenY > 10);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <nav
+      className={cn(
+        "fixed w-full z-40 transition-all duration-300",
+        isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
+      )}
+    >
+      <div className="container flex items-center justify-between">
+        <a
+          className="text-xl font-bold text-primary flex items-center"
+          href="#home"
+        >
+          <span className="relativ z-10">
+            <span className="text-glow text-foreground"> Michael's </span>{" "}
+            Portfolio
+          </span>
+        </a>
+
+        {/* desktop nav */}
+        <div className="hidden md:flex space-x-8">
+          {navItems.map((item, key) => (
+            <a
+              key={key}
+              href={item.href}
+              className="text-foreground/80 hover:text-primary transition-colors duration:300"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+
+        {/* mobile nav */}
+        <div
+          className={cn(
+            "fixed inset-0 bg-backgroun/95 backdrop-blur-md z-40 flex flex-col items-center justify-center",
+            "transition-all duration-300 md:hidden",
+            isMenuOpen
+              ? "opacity-100 pointer-events-auto"
+              : "opacity-0 pointer-events-none"
+          )}
+        >
+          <div className="hidden md:flex space-x-8">
+            {navItems.map((item, key) => (
+              <a
+                key={key}
+                href={item.href}
+                className="text-foreground/80 hover:text-primary transition-colors duration:300"
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
